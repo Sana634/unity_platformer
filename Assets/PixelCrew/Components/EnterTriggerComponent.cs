9f9765ff797dae347236089ae681e16c51601d16
SanaBu <bulavina.sana@gmail.com>
2025-09-29 01:32:04 +0300

    Добавлены файлы проекта

---
+++ Assets/PixelCrew/Components/EnterTriggerComponent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew.Components
+{
+    public class EnterTriggerComponent : MonoBehaviour
+    {
+        [SerializeField] string _tag;
+        [SerializeField] private UnityEvent _action;
+        private static int _silverCoinCount = 0;
+        private static int _goldenCoinCount = 0;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(_tag))
+            {
+                if (_action != null)
+                {
+                    _action.Invoke();
+                }
+            }
+
+        }
+
+        public void CollectSilverCoin()
+        {
+
+            _silverCoinCount += 1;
+            CalculateCoins();
+
+        }
+
+        public void CollectGoldenCoin()
+        {
+            _goldenCoinCount += 10;
+            CalculateCoins();
+
+        }
+
+        public void CalculateCoins()
+        {
+
+            int totalCoins = _goldenCoinCount + _silverCoinCount;
+            Debug.Log(totalCoins);
+        }
+    }
+}
---
+++ Assets/PixelCrew/Hero.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+
+namespace PixelCrew
+{
+    public class Hero : MonoBehaviour
+    {
+        private Rigidbody2D _rigidbody;
+        private Vector2 _movement;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _jumpSpeed;
+
+        [SerializeField] private LayerCheck _groundCheck;
+
+        public void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+        public void SetDirection(Vector2 movement)
+        {
+            _movement = movement;
+        }
+
+        public void FixedUpdate()
+        {
+            _rigidbody.velocity = new Vector2(_movement.x * _speed, _rigidbody.velocity.y);
+
+            var isJumping = _movement.y > 0.5f;
+            if (isJumping)
+            {
+                if (IsGrounded() && _rigidbody.velocity.y <= 0)
+                {
+                    _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+                }
+                else if (_rigidbody.velocity.y > 0)
+                {
+                    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+                }
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            return _groundCheck.IsTouchingLayer;
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            //Debug.DrawRay(start: transform.position, dir: Vector3.down, color: IsGrounded() ? Color.green : Color.red);
+            Gizmos.color = IsGrounded() ? Color.green : Color.red;
+            Gizmos.DrawSphere(transform.position, 0.1f);
+
+        }
+
+        public void SaySomething()
+        {
+            Debug.Log("Something!");
+        }
+    }
+}
