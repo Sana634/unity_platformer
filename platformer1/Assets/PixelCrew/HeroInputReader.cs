8aedd69f7299d72a17ade9e8bee8737ffed9addb
SanaBu <bulavina.sana@gmail.com>
2025-10-04 12:16:24 +0300

    comit
    comit

---
+++ platformer1/Assets/PixelCrew/HeroInputReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField ]private Hero _hero;
+
+    public void OnMovement (InputAction.CallbackContext context)
+    {
+        if (_hero == null) return;
+        Vector2 movement = context.ReadValue<Vector2>();
+      // Debug.Log($"Input received: {direction}");
+        _hero.SetDirection(movement);
+    }
+
+
+    public void OnSaySomething (InputAction.CallbackContext context)
+    {
+        //if (_hero == null) return;
+        if (context.canceled)
+        {
+            _hero.SaySomething();
+        }
+    }
+}
